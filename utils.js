10ca968df0512195435bcb639312f893508eb342
bunglegrind <bunglegrind@libero.it>
2025-09-24 23:36:42 +0200

    save/load preset WIP

--- utils.js
+++ utils.js
@@ -1,6 +1,13 @@
 /*jslint browser, unordered*/
 
 export default Object.freeze({
-    save: () => {},
+    save: function (event) {
+        const link = document.createElement("a");
+        const file = new Blob(["asdsadasd"], { type: 'text/plain' });
+        link.href = URL.createObjectURL(file);
+        link.download = "sample.txt";
+        link.click();
+        URL.revokeObjectURL(link.href);
+    },
     load: () => {}
 });

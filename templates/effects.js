605394c1853e5a840d157e9fd2c74de6883294b2
bunglegrind <bunglegrind@libero.it>
2025-09-26 15:18:00 +0200

    loading preset WIP

--- templates/effects.js
+++ templates/effects.js
@@ -3,63 +3,6 @@ import utils from "../utils.js";
 
 
 export default Object.freeze(function (state, dom, guitar) {
-    function popup(prompt, cb) {
-        function disable(event) {
-            const container = document.body.querySelector("#popup");
-            if (container) {
-                event.stopImmediatePropagation();
-                document.querySelector("#app").classList.remove("shadow");
-                document.body.querySelector("#popup").remove();
-                document.querySelector("#app").removeEventListener(
-                    "click",
-                    disable,
-                    true
-                );
-            }
-        }
-        document.querySelector("#app").classList.add("shadow");
-        document.querySelector("#app").addEventListener("click", disable, true);
-
-        document.body.append(dom.div("popup")(
-            dom.p("prompt")(prompt),
-            dom.input({type: "text"}),
-            dom.div({})(
-                dom.button({
-                    id: "Confirm",
-                    click: function () {
-                        const container = document.body.querySelector(
-                            "#popup"
-                        );
-                        let name = container.querySelector("input").value;
-                        if (name) {
-                            container.remove();
-                            document.querySelector("#app").classList.remove(
-                                "shadow"
-                            );
-                            document.querySelector("#app").removeEventListener(
-                                "click",
-                                disable,
-                                true
-                            );
-                            cb(name);
-                        }
-                    }
-                })("Confirm"),
-                dom.button({click: function () {
-                    const container = document.body.querySelector("#popup");
-                    document.querySelector("#app").classList.remove(
-                        "shadow"
-                    );
-                    document.querySelector("#app").removeEventListener(
-                        "click",
-                        disable,
-                        true
-                    );
-                    container.remove();
-                }})("Cancel")
-            )
-        ));
-    }
     return [
         dom.header("header")(
             dom.button({
@@ -70,18 +13,24 @@ export default Object.freeze(function (state, dom, guitar) {
                 id: "save",
                 click: function (event) {
                     event.stopImmediatePropagation();
-                    popup("Please enter a filename: ", function (name) {
+                    utils.enter_name_popup(dom, "Please enter a filename: ", function (name) {
                         utils.save(state.effects, name);
                     });
                 }
             })("Save..."),
             dom.label({
                 id: "load",
-                click: utils.load
+                change: async function ({target}) {
+                    const loaded_preset = await utils.load(target);
+                    utils.proceed_popup(dom, "The current preset configuration will be deleted. Proceed?",
+                        function () {
+                        console.log(loaded_preset);
+                        });
+                }
             })(dom.input({type: "file", accept: "application/json"}), "Load"),
             dom.button({
                 id: "undo",
-                change: utils.load
+                click: utils.load
             })("Undo"),
             dom.button({
                 id: "mixer",
--- utils.js
+++ utils.js
@@ -1,5 +1,49 @@
 /*jslint browser, unordered*/
 
+function disable(event) {
+    const container = document.body.querySelector("#popup");
+    if (container) {
+        event.stopImmediatePropagation();
+        document.querySelector("#app").classList.remove("shadow");
+        document.body.querySelector("#popup").remove();
+        document.querySelector("#app").removeEventListener(
+            "click",
+            disable,
+            true
+        );
+    }
+}
+function popup(dom, prompt, inner, callback) {
+    document.querySelector("#app").classList.add("shadow");
+    document.querySelector("#app").addEventListener("click", disable, true);
+
+    document.body.append(dom.div("popup")(
+        dom.p("prompt")(prompt),
+        inner,
+        dom.div({})(
+            dom.button({
+                id: "Confirm",
+                click: callback
+            })("Confirm"),
+            dom.button({
+                click: function () {
+                    const container = document.body.querySelector("#popup");
+                    document.querySelector("#app").classList.remove(
+                        "shadow"
+                    );
+                    document.querySelector("#app").removeEventListener(
+                        "click",
+                        disable,
+                        true
+                    );
+                    container.remove();
+                }
+            })("Cancel")
+        )
+    ));
+
+}
+
 export default Object.freeze({
     save: function (data, name) {
         const link = document.createElement("a");
@@ -12,11 +56,35 @@ export default Object.freeze({
         link.click();
         URL.revokeObjectURL(link.href);
     },
-    load: async function ({target}) {
-        const [file] = target.children[0]?.files;
+    load: async function (target) {
+        const [file] = target.files;
 
         if (file) {
-            console.log(await file.text());
+            return JSON.parse(await file.text());
         }
+    },
+    enter_name_popup: function enter_name_popup(dom, prompt, cb) {
+        popup(dom, prompt, dom.input({type: "text"}), function () {
+            const container = document.body.querySelector(
+                "#popup"
+            );
+            let name = container.querySelector("input").value;
+            if (name) {
+                container.remove();
+                document.querySelector("#app").classList.remove(
+                    "shadow"
+                );
+                document.querySelector("#app").removeEventListener(
+                    "click",
+                    disable,
+                    true
+                );
+                cb(name);
+            }
+        });
+    },
+    proceed_popup: function (dom, prompt, cb) {
+        popup(dom, prompt, dom.div({}), cb);
     }
+
 });

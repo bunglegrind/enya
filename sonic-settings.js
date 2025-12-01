adbc569cda6be826725436c07bafa749d7332638
bunglegrind <bunglegrind@libero.it>
2025-12-01 22:42:46 +0100

    eq offset and explanations

--- sonic-settings.js
+++ sonic-settings.js
@@ -3,48 +3,49 @@ const guitar = {
     name: "NOVA Go Sonic System",
     service: 0xab11,
     commands: {
-        guitar: {
+        guitar: {//guitar input volume
             opcode: 0x00,
             offset: 0,
             parameters: [{min: 0, max: 100}],
             group: "mixer"
         },
-        otg: {
+        otg: {//usb output volume
             opcode: 0x01,
             offset: 1,
             parameters: [{min: 0, max: 100}],
             group: "mixer"
         },
-        bluetooth: {
+        bluetooth: {//bluetooth input volume
             opcode: 0x02,
             offset: 2,
             parameters: [{min: 0, max: 100}],
             group: "mixer"
         },
-        box: {
+        box: {//speaker output volume
             opcode: 0x03,
             offset: 3,
             parameters: [{min: 0, max: 100}],
             group: "mixer"
         },
-        ear: {
+        ear: {//3.5mm jack output volume
             opcode: 0x04,
             offset: 4,
             parameters: [{min: 0, max: 100}],
             group: "mixer"
         },
-        line: {
+        line: {//6.35mm jack output volume
             opcode: 0x05,
             offset: 5,
             parameters: [{min: 0, max: 100}],
             group: "mixer"
         },
+        //status is on/off
         amp: {
             opcode: 0x06,
             offset: 0,
             parameters: [
                 {name: "status", min: 0, max: 1},
-                {name: "type", min: 0, max: 1},
+                {name: "type", min: 0, max: 1},//0 - clean, 1 - distorted
                 {name: "volume/preamp", min: 0, max: 100},
                 {name: "master", min: 0, max: 100},
                 {name: "bass", min: 0, max: 100},
@@ -59,12 +60,12 @@ const guitar = {
             offset: 1,
             parameters: [
                 {name: "status", min: 0, max: 1},
-                {name: "pregain", min: 0, max: 12},
-                {name: "hz80", min: 0, max: 24},
-                {name: "hz240", min: 0, max: 24},
-                {name: "hz750", min: 0, max: 24},
-                {name: "hz2200", min: 0, max: 24},
-                {name: "hz6600", min: 0, max: 24}
+                {name: "pregain", min: 0, max: 12, offset: 6},
+                {name: "hz80", min: 0, max: 24, offset: 12},
+                {name: "hz240", min: 0, max: 24, offset: 12},
+                {name: "hz750", min: 0, max: 24, offset: 12},
+                {name: "hz2200", min: 0, max: 24, offset: 12},
+                {name: "hz6600", min: 0, max: 24, offset: 12}
             ],
             group: "effects"
         },
@@ -73,6 +74,7 @@ const guitar = {
             offset: 2,
             parameters: [
                 {name: "status", min: 0, max: 1},
+                //0 - chorus, 1 - tremolo, 2 - vibrato
                 {name: "type", min: 0, max: 2},
                 {name: "depth", min: 0, max: 100},
                 {name: "rate", min: 0, max: 100}
@@ -96,7 +98,7 @@ const guitar = {
             offset: 4,
             parameters: [
                 {name: "status", min: 0, max: 1},
-                {name: "time", min: 0, max: 600},
+                {name: "time", min: 0, max: 600},//two bytes
                 {name: "level", min: 0, max: 100},
                 {name: "feedback", min: 0, max: 100}
             ],
@@ -120,7 +122,11 @@ const guitar = {
             {name: "offset-1", min: 0, max: 3},
             {name: "offset-2", min: 0, max: 3},
             {name: "offset-3", min: 0, max: 3}
-        ]}
+        ]},
+        unknown: {opcode: 0xff, parameters: [
+            {min: 0, max: 0}
+        ]}//from app to guitar. At connection startup. Seems useless
+        //TODO: factory reset
     }
 };
 
